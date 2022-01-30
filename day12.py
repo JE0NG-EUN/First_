2ac241b2b90b49c9b249c2716af699082d2a264d
Jeong Eun(Grace) <97353632+JE0NG-EUN@users.noreply.github.com>
2022-01-30 23:26:00 +0900

    Create day12.py

---
+++ day12.py
@@ -0,0 +1,80 @@
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+    .item{
+    background-color:lightgray;
+    font-weight:bold;
+    text-align:center;
+    }
+    td{
+     width:250px;
+     text-align:center;
+     }
+     .text{
+     padding:20px;
+     }
+    #right{
+    text-align:right;
+    }
+    .header{
+        height:150px;
+    }
+    </style>
+</head>
+<body>
+<table id="tabelTest" border="1">
+        <tr><th class="header" colspan="2" >
+            <img src="image2/사진.jpg" height="150px">
+        </th><td colspan="4"><b>이 력 서</b></td></tr>
+    <header class="contents">
+<tr>
+       <tr> <th class="item" rowspan="2">성명</th><td class="item">한글</td><td></td>
+        <td class="item">생년월일</td><td class="item"colspan="2">E-mail</td></tr>
+         <tr><td class="item">한문</td><td></td><td></td><td colspan="2"></td><tr>
+</tr>
+<tr>
+        <th class="item">연락처</th><td colspan="2"></td>
+        <th class="item" colspan="2">긴급연락처(핸드폰)</th><td></td>
+</tr>
+    <tr>
+        <th class="item" colspan="2">현주소</th><td colspan="4"></td>
+</tr>
+<tr>
+        <th class="item" colspan="6">학력사항</th>
+</tr>
+    <tr>
+        <th class="item">학위과정</th>
+        <th class="item">기간</th>
+        <th class="item">학교명</th>
+        <th class="item">전공</th>
+        <th class="item">수료</th>
+        <th class="item">졸업</th>
+   </tr>
+    <tr><td></td><td>~</td><td></td><td></td><td></td><td></td></tr>
+    <tr><td></td><td>~</td><td id="right">대학교</td><td></td><td></td><td></td></tr>
+    <tr><td></td><td>~</td><td id="right">대학원</td><td></td><td></td><td></td></tr>
+<tr>
+        <th class="item" colspan="6" class="Education">전공경력사항</th>
+</tr>
+    <tr>
+        <th class="item">근무연월</th>
+        <th class="item">기간</th>
+        <th class="item">근무처</th>
+        <th class="item">직위</th>
+        <th colspan="2" class="item">자격증</th>
+   </tr>
+   </tr>
+    <tr><td>년 개월</td><td>~</td><td></td><td></td><td colspan="2"></td></tr>
+    <tr><td>년 개월</td><td>~</td><td></td><td></td><td colspan="2"></td></tr>
+    <tr><td>년 개월</td><td>~</td><td></td><td></td><td colspan="2"></td></tr>
+    <tr><td>년 개월</td><td>~</td><td></td><td></td><td colspan="2"></td></tr>
+    <tr><td class="item">참고사항</td><td colspan="5"></td></tr>
+
+    <tr><td class="text" colspan="6">위 기재 사항은 사실과 틀림이 없습니다.<br><br><br><br><br>
+    20 &nbsp;&nbsp;&nbsp;년 &nbsp;&nbsp;&nbsp; 월 &nbsp;&nbsp;&nbsp; 일<br><br><br>
+    <div id="right">(인)</div></td></tr>
+    </header>
+
+</table>
+</body>
