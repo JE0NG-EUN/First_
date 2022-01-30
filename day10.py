43171e9df03940a8b163c47f31bcf50b072f1073
Jeong Eun(Grace) <97353632+JE0NG-EUN@users.noreply.github.com>
2022-01-30 23:22:50 +0900

    Create day10.py

---
+++ day10.py
@@ -0,0 +1,104 @@
+1.
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<h2>
+  	<a href="http://daum.net">다음부터</a>
+    <h2>
+        <h3>엄지용, 2014</h3>
+    <img src="image/그림.PNG" width="300" heigth="200">
+    <pre>
+이번까지만 이렇게 하고
+다음부턴 이러지 말아야지
+
+라며 버려버린 시간들이
+언젠간 한데 모여
+우린 뭐 네 인생 아니었냐고 따져 물어올 것만 같다
+</re>
+<img src="image/그림.PNG" width="300" heigth="200">
+<pre>
+시집을 받아보고 싶으면 메일주소를 써주세요!
+</re>
+</body>
+
+2.
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+     <style>
+
+    table,td{ border: 1px solid black}
+</style>
+</head>
+<body>
+<h3>테이블 구조</h3>
+<table width="400" , height="400">
+    <tr>
+        <td colspan="5" align="center">1</td>
+    </tr>
+    <tr>
+        <td rowspan="4">6</td>
+        <td colspan="4">7</td>
+
+    </tr>
+    <tr>
+        <td>12</td>
+        <td rowspan="2">13</td>
+        <td>14</td>
+        <td rowspan="3">15</td>
+    </tr>
+    <tr>
+        <td>17</td>
+        <td>19</td>
+    </tr>
+    <tr>
+        <td colspan="2">22</td>
+        <td>24</td>
+    </tr>
+</body>
+
+3.
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <style>
+    table{margin:10px; width:700px; height:200px}
+    table,td,th{border: 1px solid blue; border-collapse: collapse}
+    h1{text-align:center}
+    td{background-color:pink}
+    th{background-color:skyblue}
+    </style>
+</head>
+<body>
+<h1>24회 전국정보과학창의성대회</h1>
+<table>
+
+<tr>
+
+    <th>부문</th>
+    <td>웹프로그래밍</td>
+    <td rowspan="3" width="200" align="center">사진</td>
+
+</tr>
+<tr>
+
+    <th>수강번호</th>
+    <td>1234</td>
+
+</tr>
+<tr>
+
+    <th>성명</th>
+    <td>홍길동</td>
+
+</tr>
+<tr>
+
+    <th>좌우명</th>
+    <td colspan="2" align="center">화이팅!</td>
+
+</tr>
+
+</body>
